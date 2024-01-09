44398a3fbf097fc10684eeedbd73cfef70859a00
miguelgi266 <36167163+miguelgi266@users.noreply.github.com>
2024-01-08 23:07:01 -0800

    updated seqtagger to work with python 3.x

--- seqtagger.py
+++ seqtagger.py
@@ -1,4 +1,4 @@
-import nltk, cPickle
+import nltk, pickle
 
 
 
@@ -6,7 +6,7 @@ regexptagger = nltk.tag.RegexpTagger([ (r'^-?[0-9]+(.[0-9]+)?$', 'CD'),(r'(The|t
  (r'.*ness$', 'NN'),(r'.*ly$', 'RB'), (r'.*s$', 'NNS'), (r'.*ing$', 'VBG'), (r'.*ed$', 'VBD'), (r'.*', 'NN')])
 
  
-treebank_cutoff = len(nltk.corpus.treebank.tagged_sents()) * 3 / 4
+treebank_cutoff = len(nltk.corpus.treebank.tagged_sents()) * 3 // 4
 train_sents = nltk.corpus.treebank.tagged_sents()[:treebank_cutoff]
 treebank_test = nltk.corpus.treebank.tagged_sents()[treebank_cutoff:]
 #train_sents  = train_sents + treebank_test
@@ -17,7 +17,7 @@ tagger = nltk.UnigramTagger(train_sents, backoff = tagger)
 tagger = nltk.BigramTagger(train_sents,backoff = tagger)
 tagger = nltk.TrigramTagger(train_sents,backoff = tagger)
 
-cPickle.dump(tagger,open('pos_tagger.pickle','w'))
+pickle.dump(tagger,open('pos_tagger.pickle','wb'))
 
 
-print tagger.evaluate(treebank_test)
+print(tagger.evaluate(treebank_test))
